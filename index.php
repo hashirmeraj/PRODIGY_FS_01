a05bd2168b5177436d43e080294076902f3deb04
hashirmeraj <hashirmeraj1@gmail.com>
2024-08-28 16:29:36 +0500

    init

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
 <body style="  background-image: url(./src/images/login-bg.png); background-size:cover;">
     <div class="container">
         <form action="#" method="post">
-            <div class="login flex flex-col w-96 self-center py-2">
+            <div class="login flex flex-col w-96 self-center py-2 ">
                 <h1>Login</h1>
-                <input class=" w-60 h-8  mb-4" type="email" placeholder="Enter Your Email ID" name="loginEmail" required>
-                <input class=" w-60" type="password" placeholder="Enter Your Password" name="loginPassword" required>
+                <input class=" w-60 h-8 mb-8" type="email" placeholder="Enter Your Email ID" name="loginEmail" required>
+                <input class=" w-60 h-8 mb-4 " type="password" placeholder="Enter Your Password" name="loginPassword" required>
                 <button type="submit" class=" bg-white  w-20 h-9">Login</button>
                 <span>Don't have account? <span class=" font-semibold"><a href="./register.php">Register</a></span></span>
             </div>
